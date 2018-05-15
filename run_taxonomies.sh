e6aae14e192939cb39dc6bd22924670f37243212
Hiren Joshi <hirenj@gmail.com>
2018-05-15 17:50:01 +0200

    Show start of uniprot

--- run_taxonomies.sh
+++ run_taxonomies.sh
@@ -13,6 +13,7 @@ for taxid in ${taxids//,/ }; do
 	bash ./retrieve_glycoinfo.sh $taxid > sources/glycosylation.tsv
 	runrecipe --input sources --output json --env version="$TARGETVERSION" --env git="$GIT_STATUS" --env timestamp="$(date -u +%FT%TZ)" --env taxid="$taxid"
 	mv json/sources_glycosylation.json dist/glycosylation-$taxid.json
+	head -3 dist/glycosylation-$taxid.json
 done
 
 bash ./retrieve_mappings.sh > sources/mappings.tsv
