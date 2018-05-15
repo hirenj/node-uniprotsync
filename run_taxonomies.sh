97e3c0c14eca75f95d5e32e7adf4b8dbddc41e7b
Hiren Joshi <hirenj@gmail.com>
2018-05-15 17:21:22 +0200

    Stop running the refseq mapping multiple times

--- run_taxonomies.sh
+++ run_taxonomies.sh
@@ -6,12 +6,15 @@ if [ ! -e dist ]; then
 	mkdir dist;
 fi
 
-bash ./retrieve_mappings.sh > sources/mappings.tsv
-
 for taxid in ${taxids//,/ }; do
 	echo "Retrieving glycosylation data for $taxid"
 	bash ./retrieve_glycoinfo.sh $taxid > sources/glycosylation.tsv
 	runrecipe --input sources --output json --env version="$TARGETVERSION" --env git="$GIT_STATUS" --env timestamp="$(date -u +%FT%TZ)" --env taxid="$taxid"
 	mv json/sources_glycosylation.json dist/glycosylation-$taxid.json
-	mv json/sources_refseqnt.json dist/refseqnt.json
 done
+
+bash ./retrieve_mappings.sh > sources/mappings.tsv
+
+runrecipe --input sources --output json --env version="$TARGETVERSION" --env git="$GIT_STATUS" --env timestamp="$(date -u +%FT%TZ)" --env taxid="$taxid"
+
+mv json/sources_refseqnt.json dist/refseqnt.json
