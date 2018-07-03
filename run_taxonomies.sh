e5f775c2d889d2e672a4b759a3324d7ef0fc6ae7
Hiren Joshi <hirenj@gmail.com>
2018-07-03 15:23:18 +0200

    https fix for uniprot

--- run_taxonomies.sh
+++ run_taxonomies.sh
@@ -12,6 +12,9 @@ for taxid in ${taxids//,/ }; do
 	echo "Retrieving glycosylation data for $taxid"
 	bash ./retrieve_glycoinfo.sh $taxid > sources/glycosylation.tsv
 	runrecipe --input sources --output json --env version="$TARGETVERSION" --env git="$GIT_STATUS" --env timestamp="$(date -u +%FT%TZ)" --env taxid="$taxid"
+	if [ "$?" -gt 0 ]; then
+		exit 1
+	fi
 	mv json/sources_glycosylation.json dist/glycosylation-$taxid.json
 	head -3 dist/glycosylation-$taxid.json
 done
