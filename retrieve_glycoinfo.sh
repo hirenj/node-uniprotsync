f3bca45e192b6f01815b98fa4e5be2b866fbda58
Hiren Joshi <hirenj@gmail.com>
2019-08-18 21:18:01 +0200

    Fix for only using experimental data

--- retrieve_glycoinfo.sh
+++ retrieve_glycoinfo.sh
@@ -5,7 +5,7 @@ taxids=$1
 echo "uniprot	glyco"
 for taxid in ${taxids//,/ }; do
 	url="https://www.uniprot.org/uniprot/?sort=score&desc=&compress=no&query=organism:$taxid%20AND%20annotation:(type:carbohyd%20evidence:ECO_0000269)&fil=&force=no&format=tab&columns=id,feature(GLYCOSYLATION)"
-	curl $url | tail -n+2
+	curl $url | tail -n+2 | sed -e 's/e;/e/g'
 	if [ $? -gt 0 ]; then
 		exit 1
 	fi
