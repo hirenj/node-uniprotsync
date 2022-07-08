0ff2d6323786b80c08da3ac2878645a9a39a7964
Hiren Joshi <hirenj@gmail.com>
2022-07-08 13:09:49 +0200

    Fix URL for retrieving uniprot

--- retrieve_glycoinfo.sh
+++ retrieve_glycoinfo.sh
@@ -4,7 +4,7 @@ taxids=$1
 
 echo "uniprot	glyco"
 for taxid in ${taxids//,/ }; do
-	url="https://www.uniprot.org/uniprot/?sort=score&desc=&compress=no&query=organism:$taxid%20AND%20annotation:(type:carbohyd%20evidence:ECO_0000269)&fil=&force=no&format=tab&columns=id,feature(GLYCOSYLATION)"
+	url="https://rest.uniprot.org/uniprotkb/stream?fields=accession,ft_carbohyd&format=tsv&query=organism_id:$taxid%20AND%20(ftev_carbohyd:ECO_0000269)"
 	curl $url | tail -n+2 | sed -e 's/e;/e/g'
 	if [ $? -gt 0 ]; then
 		exit 1
