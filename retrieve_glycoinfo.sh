4416885ed8a4a929ea90ba64bb5b2ad46a0930f2
Hiren Joshi <hirenj@gmail.com>
2023-09-20 09:51:11 +0200

    Fix for retrieving UniProt data by evidence code

--- retrieve_glycoinfo.sh
+++ retrieve_glycoinfo.sh
@@ -4,7 +4,7 @@ taxids=$1
 
 echo "uniprot	glyco"
 for taxid in ${taxids//,/ }; do
-	url="https://rest.uniprot.org/uniprotkb/stream?fields=accession,ft_carbohyd&format=tsv&query=organism_id:$taxid%20AND%20(ftev_carbohyd:ECO_0000269)"
+	url="https://rest.uniprot.org/uniprotkb/stream?fields=accession,ft_carbohyd&format=tsv&query=organism_id:$taxid%20AND%20(ft_carbohyd_exp:*)"
 	curl $url | tail -n+2 | sed -e 's/e;/e/g'
 	if [ $? -gt 0 ]; then
 		exit 1
